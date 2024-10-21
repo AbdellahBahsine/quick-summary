bc113d24341e924d1d6c61ba882e5cb0deb35636
Abdellah Bahsine <69261429+AbdellahBahsine@users.noreply.github.com>
2024-10-21 16:31:36 +0100

    feat: Create new summary form page

---
+++ frontend/src/app/my-summaries/new-summary/page.tsx
@@ -0,0 +1,14 @@
+import NewSummaryForm from "./components/NewSummaryForm/NewSummaryForm";
+
+const NewSummaryPage = () => {
+    return (
+        <div className="px-6 md:px-10 flex justify-center">
+            <div className="max-w-[1545px] w-full">
+                <h1 className="font-bold text-xl my-6">New Summary</h1>
+                <NewSummaryForm />
+            </div>
+        </div>
+    );
+}
+
+export default NewSummaryPage;
